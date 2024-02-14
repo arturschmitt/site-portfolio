1046ad418b3da3caf349ddf98252acbdbfe07656
arturschmitt <86473243+arturschmitt@users.noreply.github.com>
2024-02-14 15:45:42 -0300

    Add files via upload

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,9 +1,11 @@
 import style from "./style.css"
+import Link from "next/link"
 
 export default function Footer(){
     return(
         <footer>
-            <h1>Artur Brenner Schmitt</h1>
+            <h1><Link href="/" className="link-style">Artur Brenner Schmitt</Link></h1>
+            <p>Desenvolvido com NextJS</p>
         </footer>
     )
 }
